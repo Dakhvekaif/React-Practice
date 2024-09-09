e9a60e79bbb3fbe0102e6353ed1f096535756287
Dakhve Kaif <kaifdaakve@gmail.com>
2024-09-09 21:43:11 +0530

    Added new files like Postcard RTE,login,input authLayout and much more

--- 12MegaBlog/src/components/Header/LogoutBtn.jsx
+++ 12MegaBlog/src/components/Header/LogoutBtn.jsx
@@ -11,7 +11,7 @@ function LogoutBtn() {
         }).catch((error) => console.log("Error at logoutHandler", error) )
     }
   return (
-    <div>LogoutBtn</div>
+    <button className='inline-bock px-6 py-2 duration-200 hover:bg-blue-100 rounded-full'>Logout</button>
   )
 }
 
--- 12MegaBlog/src/components/index.js
+++ 12MegaBlog/src/components/index.js
@@ -1,5 +1,9 @@
 import Header from "./Header/Header";
 import Footer from "./Footer/Footer";
+import container from './container/container.jsx';
+import Logo from "./Logo";
+import LogoutBtn from "./Header/LogoutBtn";
+import RTE from "./RTE.jsx";
 
 
-export { Header, Footer }
+export { Header, Footer , container, Logo, LogoutBtn, RTE}
